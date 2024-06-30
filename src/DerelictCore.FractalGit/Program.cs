3434ac63f002514ab4dbf9d982cc182e6eca8822
Sára El-Saig <sara.el-saig@lombiq.com>
2024-06-30 11:49:03 +0200

    Add top button control bar with FontAwesome 6 icons.

--- src/DerelictCore.FractalGit/Program.cs
+++ src/DerelictCore.FractalGit/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 using System;
 using System.Diagnostics.CodeAnalysis;
 
@@ -11,17 +13,23 @@ public static class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
 
     [SuppressMessage(
         "ReSharper",
         "MemberCanBePrivate.Global",
         Justification = "Avalonia configuration, don't remove; also used by visual designer.")]
-    public static AppBuilder BuildAvaloniaApp() =>
-        AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        IconProvider.Current
+            .Register<FontAwesomeIconProvider>();
+
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
+    }
 }
--- src/DerelictCore.FractalGit/ViewModels/MainWindowViewModel.cs
+++ src/DerelictCore.FractalGit/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,7 @@
 
 public class MainWindowViewModel : ViewModelBase
 {
+    public string Filter { get; set; } = string.Empty;
     public GitGraphViewModel Graph { get; set; } = new();
+    public int ChangedFileCount { get; set; }
 }
